d0e5c37bcfedd399dc46c59f917cf71756bfa4d9
JeniferDenise <espeche.jenifer@tsoftglobal.com>
2023-03-14 12:24:32 -0300

    agrego reporte qa

--- tarea.sh
+++ tarea.sh
@@ -10,8 +10,8 @@ echo $token
 curl --location 'https://xray.cloud.getxray.app/api/v2/import/execution/cucumber/multipart' \
 --header 'Content-Type: multipart/form-data' \
 --header "Authorization: Bearer $token" \
---form info=@"/C:/Users/jenifer.espeche/Desktop/BashTareas/info.json" \
---form results=@"/C:/Users/jenifer.espeche/Desktop/BashTareas/Cucumberprueba.json">peticion.txt
+--form info=@"info.json" \
+--form results=@"Cucumberprueba.json">peticion.txt
 export key=$(grep "IXR" peticion.txt | cut -d '"' -f 8)
 
 echo $key
@@ -19,4 +19,4 @@ echo $key
 curl --location -g "https://integracionx-ray.atlassian.net/rest/api/3/issue/$key/attachments" \
 --header 'X-Atlassian-Token: no-check' \
 --header 'Authorization: Basic amVuaWZlci5lc3BlY2hlQHRzb2Z0bGF0YW0uY29tOkFUQVRUM3hGZkdGMGRsb0VFY1FpNkl1VENZcU5XdzdMV0lQSFd5SWxEMjRYdDhPbW9SSU1LQ1RZN1N4cHIxT2hHQlZacWpiNnFaTlk1c0NEOVcydlR2REJpUnlHeFduSTlTRDBCbjRGUTVCTDRlSTEwTkZMdU13dkNyR3dvNWJ6cV8xWEdtRU8xZ0xKTmMtRFZCcmJqOHhQZ0FRNW1fV2hJQTBtQ2tDVzhmTmgxMExwT24tY1d2OD1DREU2QUIzMQ==' \
---form file=@"/C:/Users/jenifer.espeche/Downloads/reporteQA.pdf"
+--form file=@"reporteQA.pdf"
